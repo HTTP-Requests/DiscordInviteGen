99adea838bcfbf5071af870f6adfda2c6c3d34ca
HTTP Request <95198803+HTTP-Requests@users.noreply.github.com>
2022-07-13 18:43:29 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+namespace DiscordInviteGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Time:
+            Console.WriteLine("Input the time (In MS) you want the code's to be delayed. Input 0 for no delay.");
+            var time = Console.ReadLine();
+            bool isInt = time.All(char.IsDigit);
+            if (isInt == false)
+            {
+                Console.WriteLine("You need to input a number");
+                goto Time;
+            }
+            int sleep = Convert.ToInt32(time);
+        Gen:
+            Random random = new Random();
+            string url = "https://discord.gg/";
+            string link = "";
+
+            string dict = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+
+            for (int i = 0; i < 8; i++)
+            {
+                link += dict[random.Next(0, dict.Length - 1)];
+            }
+
+            string server = url + link;
+            Console.WriteLine(server);
+            System.Threading.Thread.Sleep(sleep);
+            goto Gen;
+
+        }
+    }
+}
